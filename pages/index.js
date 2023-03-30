9a39c13397a0a03909dcadb3f51b62a68d00ef91
Umesh Kumar <uk481281@gmail.com>
2023-03-30 19:40:02 +0530

    title

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,14 @@ import HeroBanner from '@/components/HeroBanner';
 import Navbar from '@/components/Navbar'
 import TechStack from '@/components/TechStack';
 import Contact from '@/components/Contact';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Umesh Kumar</title>
+      </Head>
       <Navbar />
       <HeroBanner />
       <TechStack />
